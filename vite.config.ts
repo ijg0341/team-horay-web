d419789eae145e9c85df48c69d3e4e73aea28c5b
JinGyu Lim <ijg0341@naver.com>
2024-09-12 10:22:48 +0900

    build setting

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/team-horay-web/',
   build: { outDir: 'build' },
   resolve: {
     alias: {
